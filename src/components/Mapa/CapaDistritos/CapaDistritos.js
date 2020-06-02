6899e429be5ca1af14741ae9bc9d8c0272c89991
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-06-01 23:37:05 -0400

    orden de codigo

--- src/components/Mapa/CapaDistritos/CapaDistritos.js
+++ src/components/Mapa/CapaDistritos/CapaDistritos.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Source, Layer } from 'react-map-gl'
 import './CapaDistritos.css'
 import geoJSONDistritos from '../../../data/geojson/distritos.json'
 
 const CapaDistritos = () => {
 
+  const geoJSONProcesado = useMemo(() => ({
+    ...geoJSONDistritos,
+    features: geoJSONDistritos.features.map(f => ({
+      ...f,
+      properties: {
+        ...f.properties,
+        x: Math.random()
+      }
+    }))
+  }), [])
+
   return (
     <Source
       id="capa-datos-distritos"
       type="geojson"
-      data={geoJSONDistritos}
+      data={geoJSONProcesado}
     >
       <Layer
         id="distritos-fill"
         type="fill"
         paint={{
-          'fill-color': 'rgba(255, 255, 255, .5)'
+          "fill-color": {
+            property: 'x',
+            stops: [
+              [0, '#FFFFCE'],
+              [0.25, '#C8E8B6'],
+              [0.35, '#83CCBB'],
+              [0.65, '#48B6C2'],
+              [0.85, '#3180B6'],
+              [1, '#273891']
+            ]
+          }//['to-color', ['at', 0, ['get', 'colores']]],
+        }}
+      />
+      <Layer
+        id="distritos-line"
+        type="line"
+        paint={{
+          'line-color': 'rgba(255, 255, 255, 1)',
+          'line-width': .5
         }}
       />
     </Source>
